ad8e7d5b4d69e067285b3fc1cd819f94fd7672ff
jujusinhodoscapa <166841741+jujusinhodoscapa@users.noreply.github.com>
2024-06-14 14:00:16 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+let temperatura = 0
+input.onButtonPressed(Button.A, function () {
+    temperatura = input.temperature()
+    basic.showString("" + temperatura + "c")
+})
+basic.forever(function () {
+	
+})
